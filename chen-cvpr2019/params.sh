05acd153ccccda1521f0a1acf38a4695f74bb8c9
Yusuke Kondo <konyou@g.ecc.u-tokyo.ac.jp>
2021-03-29 17:08:30 +0900

    fix: do not use privilege param (--ipc=all)

--- chen-cvpr2019/params.sh
+++ chen-cvpr2019/params.sh
@@ -4,8 +4,6 @@ cd $(dirname $0)
 NAME="chen-cvpr2019"
 IMAGENAME="${NAME}"
 CONTNAME="--name=${NAME}"
-NET="--net=host"
-IPC="--ipc=host"
 GPU="--gpus all"
 
 VOLUMES="-v $(pwd)/src:/workspace/ -v $(pwd)/data:/data/"
